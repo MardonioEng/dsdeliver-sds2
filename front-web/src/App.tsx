f3ab5e554d44eea7270e21e1e49ac5652f585038
MardonioEng <mardoniorodrigues11@gmail.com>
2021-01-06 15:08:58 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './NavBar';
+
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <>
+        <Navbar />
+        <Home />
+      </>
     </div>
   );
 }
